34b39174c8b17e3f4bdeae023c640b1a04e67e5a
zhuhd <zhuhd@kaopu.com>
2022-12-05 18:41:40 +0800

    first commit

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
