95a0e2f6a7e9a76a5859995b3f71b1b22a3b684e
zhuhd <zhuhd@kaopu.com>
2022-12-07 09:46:16 +0800

    first day

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    post_list = Post.objects.all().order_by('-created')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
